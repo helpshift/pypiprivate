dc67a3477f0832893805ee13ca5fc98851647211
Vineet Naik <vineet@helpshift.com>
2018-10-05 13:54:39 +0530

    Add License and update setup.py
    Change-Id: I23a7fab75c5eff42199636e0a375dfad7b8d1740

--- setup.py
+++ setup.py
@@ -10,14 +10,19 @@ with open('pypiprivate/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1)))
 
 
+with open('./README.rst') as f:
+    long_desc = f.read()
+
+
 setup(
     name='pypiprivate',
     version=version,
     author='Vineet Naik',
     author_email='vineet@helpshift.com',
     url='http://helpshift.com',
-    license='Proprietary',
+    license='MIT License',
     description='Private package management tool for Python projects',
+    long_description=long_desc,
     install_requires=['Jinja2==2.10.0',
                       'boto3==1.5.27'],
     packages=['pypiprivate'],
@@ -25,5 +30,14 @@ setup(
         'console_scripts': [
             'pypi-private = pypiprivate.cli:main'
         ]
-    }
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
